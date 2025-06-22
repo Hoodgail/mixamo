d884a2b06063906f5b9b8b6af0fa0d2797a4aa51
Drex Benjamin <drex.ben@outlook.com>
2025-06-22 15:06:25 -0400

    Clean

--- index.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { runDownloader } from './src';
-
-// Run the main function
-runDownloader().catch(error => {
-     console.error('Unhandled error:', error);
-     process.exit(1);
-});
